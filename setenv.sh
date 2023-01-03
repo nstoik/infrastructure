d0ed76e31f02763e97cafb4c7210fb1bf8cea866
Nelson Stoik <nelsonstoik@gmail.com>
2023-01-02 21:02:03 -0700

    get token from ENV instead of script

---
+++ setenv.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# Set environment variables for the current shell.
+# The source of the environment variables is a file named .env in the current directory.
+
+set -a
+source .env
+set +a
